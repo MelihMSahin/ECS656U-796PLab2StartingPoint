735cbc814c0dd65377fc05a6c9ec0f7554077b54
Melih Mehmet Sahin <msahin8765@gmail.com>
2025-04-06 20:17:48 +0100

    Update GRPCClientService.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -1,5 +1,4 @@
 package com.example.grpc.client.grpcclient;
- 
 import com.example.grpc.server.grpcserver.PingRequest;
 import com.example.grpc.server.grpcserver.PongResponse;
 import com.example.grpc.server.grpcserver.PingPongServiceGrpc;
@@ -13,54 +12,59 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 @Service
 public class GRPCClientService {
-    public String ping() {
-        	ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-                .usePlaintext()
-                .build();        
-		PingPongServiceGrpc.PingPongServiceBlockingStub stub
-                = PingPongServiceGrpc.newBlockingStub(channel);        
-		PongResponse helloResponse = stub.ping(PingRequest.newBuilder()
-                .setPing("")
-                .build());        
-		channel.shutdown();        
-		return helloResponse.getPong();
-    }
-    public String add(){
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
-		.usePlaintext()
-		.build();
-		MatrixServiceGrpc.MatrixServiceBlockingStub stub
-		 = MatrixServiceGrpc.newBlockingStub(channel);
-		MatrixReply A=stub.addBlock(MatrixRequest.newBuilder()
-			.setA00(1)
-			.setA01(2)
-			.setA10(5)
-			.setA11(6)
-			.setB00(1)
-			.setB01(2)
-			.setB10(5)
-			.setB11(6)
-			.build());
-		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
-		return resp;
-    }
+public String ping() {
+ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",
+9090)
+.usePlaintext()
+.build();
+PingPongServiceGrpc.PingPongServiceBlockingStub stub
+= PingPongServiceGrpc.newBlockingStub(channel);
+PongResponse helloResponse = stub.ping(PingRequest.newBuilder()
+.setPing("")
+.build());
+channel.shutdown();
+return helloResponse.getPong();
+}
+public String add(){
+ManagedChannel channel =
+ManagedChannelBuilder.forAddress("localhost",9090)
+.usePlaintext()
+.build();
+MatrixServiceGrpc.MatrixServiceBlockingStub stub
+= MatrixServiceGrpc.newBlockingStub(channel);
+MatrixReply A=stub.addBlock(MatrixRequest.newBuilder()
+.setA00(1)
+.setA01(2)
+.setA10(5)
+.setA11(6)
+.setB00(1)
+.setB01(2)
+.setB10(5)
+.setB11(6)
+.build());
+String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+"
+"+A.getC11()+"\n";
+return resp;
+}
 public String multiply(){
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
-		.usePlaintext()
-		.build();
-		MatrixServiceGrpc.MatrixServiceBlockingStub stub
-		 = MatrixServiceGrpc.newBlockingStub(channel);
-		MatrixReply A=stub.multiplyBlock(MatrixRequest.newBuilder()
-			.setA00(1)
-			.setA01(2)
-			.setA10(5)
-			.setA11(6)
-			.setB00(1)
-			.setB01(2)
-			.setB10(5)
-			.setB11(6)
-			.build());
-		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
-		return resp;
-    }
+ManagedChannel channel =
+ManagedChannelBuilder.forAddress("localhost",9090)
+.usePlaintext()
+.build();
+MatrixServiceGrpc.MatrixServiceBlockingStub stub
+= MatrixServiceGrpc.newBlockingStub(channel);
+MatrixReply A=stub.multiplyBlock(MatrixRequest.newBuilder()
+.setA00(1)
+.setA01(2)
+.setA10(5)
+.setA11(6)
+.setB00(1)
+.setB01(2)
+.setB10(5)
+.setB11(6)
+.build());
+String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+"
+"+A.getC11()+"\n";
+return resp;
+}
 }
